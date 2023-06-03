3908fdb792e99b9b3e0302023983e65e9a680711
polosa00 <i@apolochanin.ru>
2023-06-03 15:51:13 +0300

    Initial Commit

---
+++ Firebase-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase-Sample
+//
+//  Created by Александр Полочанин on 3.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
